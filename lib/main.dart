1c99663f22dc2ef6997f33ef344ee7c0fc32c45f
aditi-g21 <goyal.aditi121@gmail.com>
2020-03-03 22:38:25 +0530

    Adding xylophone code

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int num){
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Expanded expandKey(Color color, int sound){
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(sound);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              expandKey(Colors.amber[100], 1),
+              expandKey(Colors.amber[200], 2),
+              expandKey(Colors.amber[300], 3),
+              expandKey(Colors.amber[400], 4),
+              expandKey(Colors.amber[500], 5),
+              expandKey(Colors.amber[600], 6),
+              expandKey(Colors.amber[700], 7),
+            ],
+          ),
         ),
       ),
     );
